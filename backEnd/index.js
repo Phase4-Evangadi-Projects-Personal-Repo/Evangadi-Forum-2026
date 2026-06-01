cd9ae709fc88c9a64d5bb655a348e9b285eb480b
RediatAndualem <rediatandualeme@gmail.com>
2026-06-02 04:18:47 +0530

    project initiated

---
+++ backEnd/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const db = require("./schema/db.config");
+const mainRoutes = require("./src/api/main.routes");
+const { errorHandler } = require("./src/middleware/error-handler");
+
+//! middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+//! dotenv initiation
+const dotenv = require("dotenv");
+dotenv.config();
+
+//!Main api
+app.use("/api", mainRoutes);
+
+// ! your error handler middleware should be after all api calls
+app.use(errorHandler);
+
+//! connection and server configuration
+
+async function startServer() {
+  try {
+    const connection = await db.getConnection();
+    connection.release();
+    console.log("Connected to database");
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+startServer();
---
+++ backEnd/schema/db.config.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql2/promise");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  charset: "utf8mb4",
+});
+
+module.exports = db;
